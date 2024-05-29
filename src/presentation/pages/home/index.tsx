ba0cfa9b1803893c67c3664b8d6215b79b2850d0
Willian Igor <willanigordeveloper@gmail.com>
2024-05-29 09:09:14 -0300

    Improve format/structure: update homepage to be responsive

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -1,16 +1,14 @@
 export function HomePage() {
   return (
-    <main>
-      <div className="container text-center p-5 h-screen flex flex-col items-center justify-center gap-3 max-w-sm md:flex-row-reverse md:text-left md:max-w-none md:gap-5 md:h-[80vh] lg:gap-8 xl:gap-28">
-        <img src="/icon.svg" alt="Atlas" className="md:max-w-xs" />
-        <div className="space-y-3 md:max-w-xs lg:max-w-md xl:max-w-xl">
-          <h2 className="text-3xl p-1 font-serif font-black text-primary lg:text-4xl xl:text-6xl">
-            Mestre das Masmorras
-          </h2>
-          <p className="px-3 md:p-0">
-            Tudo o que você precisa para criar e gerenciar suas aventuras de RPG.
-          </p>
-        </div>
+    <main className="container text-center py-20 px-5 flex flex-col items-center justify-center gap-3 max-w-sm md:flex-row-reverse md:text-left md:max-w-none md:gap-5 md:h-[80vh] lg:gap-8 xl:gap-28">
+      <img src="/icon.svg" alt="Atlas" className="md:max-w-xs" />
+      <div className="space-y-3 md:max-w-xs lg:max-w-md xl:max-w-xl">
+        <h2 className="text-3xl p-1 font-serif font-black text-primary sm:text-4xl md:text-3xl lg:text-5xl xl:text-6xl">
+          Mestre das Masmorras
+        </h2>
+        <p className="px-3 md:p-0">
+          Tudo o que você precisa para criar e gerenciar suas aventuras de RPG.
+        </p>
       </div>
     </main>
   )
