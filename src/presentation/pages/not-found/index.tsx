a251d6c661a29e0916ec0ef65851839bd119758d
Willian Igor <willianigordeveloper@gmail.com>
2024-06-02 11:45:38 -0300

    Improve format/structure: not found page

--- src/presentation/pages/not-found/index.tsx
+++ src/presentation/pages/not-found/index.tsx
@@ -1,9 +1,15 @@
-import { Button } from "@/presentation/components/ui/button"
-import { ROUTES } from "@/utils/routes"
-import { ChevronsLeftIcon } from "lucide-react"
+// ==== Imports ====
+// Externals
 import { Link } from "react-router-dom"
+import { ChevronsLeftIcon } from "lucide-react"
+// Utils
+import { ROUTES } from "@/utils/routes"
+// Components
+import { Button } from "@/presentation/components/ui/button"
 
+// ==== Component ====
 export function NotFoundPage() {
+  // Return
   return (
     <div className="h-svh flex flex-col items-center justify-center gap-3 animate-fade-down duration-500">
       <h1 className="text-9xl font-serif text-primary">404</h1>
